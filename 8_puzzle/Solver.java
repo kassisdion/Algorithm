0e6141eb12cbd2c912641809e6b6d65171a81153
florian <florian.faisant@gmail.com>
2016-05-02 12:10:06 +0900

    test thread

--- 8_puzzle/Solver.java
+++ 8_puzzle/Solver.java
@@ -1,11 +1,11 @@
 /**
  * Homework Assignment #6: "8-Puzzle"
- *
- *  - Solver class for solving "8-Puzzle" Programming Assignment
- *
- *  Compilation:  javac Solver.java Board.java
- *  Execution:    java Solver inputfile.txt
- *  Dependencies: MinPQ
+ * <p>
+ * - Solver class for solving "8-Puzzle" Programming Assignment
+ * <p>
+ * Compilation:  javac Solver.java Board.java
+ * Execution:    java Solver inputfile.txt
+ * Dependencies: MinPQ
  *
  * @ Student ID : 50151252
  * @ Name       : Faisant Florian
@@ -18,11 +18,10 @@ import java.util.Scanner;
 
 public class Solver {
 
-    private boolean solved;
-    private Node solution;
+    private Node mSolution;
 
     // search node
-    private class Node implements Comparable<Node> {
+    private static class Node implements Comparable<Node> {
         private Board board;
         private int moves;
         private Node prev;
@@ -47,13 +46,60 @@ public class Solver {
         }
     }
 
+
+    //Thread for Solver
+    private static class SolverTask extends Thread {
+
+        private interface ThreadCompleteListener {
+            void notifyOfThreadComplete(Node solution);
+        }
+
+        private MinPQ<Node> pq;
+        private Node node;
+        private List<Board> save = new LinkedList<>();
+        private ThreadCompleteListener listener;
+
+        SolverTask(MinPQ<Node> pq, Node startingNode) {
+            this.pq = pq;
+            this.node = startingNode;
+            this.listener = null;
+        }
+
+        void setListener(ThreadCompleteListener listener) {
+            this.listener = listener;
+        }
+
+        public void run() {
+            Boolean running = true;
+            while (running && !this.isInterrupted()) {
+                if (node.board.isGoal()) {
+                    listener.notifyOfThreadComplete(node);
+                    running = false;
+                } else {
+                    node = step(pq, save);
+                }
+            }
+        }
+
+        private Node step(MinPQ<Node> pq, List<Board> save) {
+            Node least = pq.delMin();
+            for (Board neighbor : least.board.neighbors()) {
+                if (least.prev == null || !neighbor.equals(least.prev.board)) {
+                    if (!save.contains(neighbor)) {
+                        pq.insert(new Node(neighbor, least.moves + 1, least));
+                        save.add(neighbor);
+                    }
+                }
+            }
+            return least;
+        }
+    }
+
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null)
             throw new java.lang.NullPointerException();
-
-        solved = false;
-        solution = null;
 
         // create initial search node (and it's twin)
         Node startingNode = new Node(initial, 0, null);
@@ -63,59 +109,58 @@ public class Solver {
         MinPQ<Node> mainPq = new MinPQ<>();
         MinPQ<Node> twinPq = new MinPQ<>();
 
-        //list for saving visited board
-        List<Board> mainSave = new LinkedList<>();
-        List<Board> twinSave =  new LinkedList<>();
-
         // insert the initial search node into a priority queue
         mainPq.insert(startingNode);
         twinPq.insert(twinNode);
 
+        // instantiate 2 Thread
+        final SolverTask mainSolver = new SolverTask(mainPq, startingNode);
+        final SolverTask twinSolver = new SolverTask(twinPq, twinNode);
+
+        mainSolver.setListener(new SolverTask.ThreadCompleteListener() {
+            @Override
+            public void notifyOfThreadComplete(Node solution) {
+                twinSolver.interrupt();
+                mSolution = solution;
+            }
+        });
+
+        twinSolver.setListener(new SolverTask.ThreadCompleteListener() {
+            @Override
+            public void notifyOfThreadComplete(Node solution) {
+                mainSolver.interrupt();
+                mSolution = solution;
+            }
+        });
+
         // solve the puzzle
-        while (!solved) {
-            if (startingNode.board.isGoal()) {
-                solution = startingNode;
-                solved = true;
-            }
+        mainSolver.start();
+        twinSolver.start();
 
-            if (twinNode.board.isGoal()) {
-                solution = null;
-                solved = true;
-            }
-
-            startingNode = step(mainPq, mainSave);
-            twinNode = step(twinPq, twinSave);
+        try {
+            mainSolver.join();
+            twinSolver.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
-
-    private Node step(MinPQ<Node> pq, List<Board> save) {
-        Node least = pq.delMin();
-        for (Board neighbor : least.board.neighbors()) {
-            if (least.prev == null || !neighbor.equals(least.prev.board)) {
-                if (!save.contains(neighbor)) {
-                    pq.insert(new Node(neighbor, least.moves + 1, least));
-                    save.add(neighbor);
-                }
-            }
-        }
-        return least;
     }
 
     // is the initial board solvable?
     private boolean isSolvable() {
-        return solution != null;
+        return mSolution != null;
     }
+
     // min number of moves to solve initial board; -1 if unsolvable
     private int moves() {
-        return solution == null ? -1 : solution.moves;
+        return mSolution == null ? -1 : mSolution.moves;
     }
 
     // sequence of boards in a intest solution; null if unsolvable
     private Iterable<Board> solution() {
-        if (solution == null)
+        if (mSolution == null)
             return null;
         Stack<Board> sol = new Stack<>();
-        Node searchNode = solution;
+        Node searchNode = mSolution;
         while (searchNode != null) {
             sol.push(searchNode.board);
             searchNode = searchNode.prev;
@@ -147,7 +192,7 @@ public class Solver {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 blocks[i][j] = in.nextInt();
-                if (blocks[i][j] >= N*N)
+                if (blocks[i][j] >= N * N)
                     throw new IllegalArgumentException("value must be < N^2");
                 if (blocks[i][j] < 0)
                     throw new IllegalArgumentException("value must be >= 0");
@@ -171,7 +216,7 @@ public class Solver {
 
         // calculate running time
         time = (System.currentTimeMillis() - start) / 1000.0;
-        System.out.println("time = "+ time + "sec");
+        System.out.println("time = " + time + "sec");
         System.out.println("Minimum number of moves = " + solver.moves() + "\n");
     }
 }
